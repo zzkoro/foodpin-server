8b7897188507e0bcd703300459e2a2497a33f49f
junemp <junemp@gmail.com>
2019-08-26 07:50:54 +0900

    - commit

---
+++ src/main/java/com/zzkoro/foodpinsvr/FoodpinsvrApplication.java
@@ -0,0 +1,13 @@
+package com.zzkoro.foodpinsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodpinsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodpinsvrApplication.class, args);
+    }
+
+}
